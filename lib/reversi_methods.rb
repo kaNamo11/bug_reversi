b1d9cdf3a511c2c082ea501858e41562b522c1df
kaNamo <yum.kanamori@gmail.com>
2025-08-21 14:21:39 +0900

    不要な差分を削除

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -54,6 +54,7 @@ module ReversiMethods
       next_pos = pos.next_position(direction)
       turn_succeed = true if turn(copied_board, next_pos, stone_color, direction)
     end
+
     copy_board(board, copied_board) if !dry_run && turn_succeed
 
     turn_succeed
@@ -63,6 +64,7 @@ module ReversiMethods
     return false if target_pos.out_of_board?
     return false if target_pos.stone_color(board) == attack_stone_color
     return false if target_pos.stone_color(board) == BLANK_CELL
+
     next_pos = target_pos.next_position(direction) 
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
       board[target_pos.row][target_pos.col] = attack_stone_color
@@ -80,6 +82,7 @@ module ReversiMethods
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         next unless cell == BLANK_CELL
+
         position = Position.new(row, col)
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
